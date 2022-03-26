898229c1b659a87291ed1189eeb2cce8c66082fb
Bruno Bessa Chaves <bruno.chaves@bluemedsaude.com.br>
2022-03-26 16:35:17 -0300

    Changing the layout of listing pages, creating and editing posts

--- blog/app/Views/posts/create.php
+++ blog/app/Views/posts/create.php
@@ -4,53 +4,57 @@
         <div class="row">
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 mt-3">
                 <nav aria-label="breadcrumb">
-                    <ol class="breadcrumb">
+                    <ol class="breadcrumb bg-light">
                     <li class="breadcrumb-item" aria-current="page">List of registered posts</li>
                         <li class="breadcrumb-item active" aria-current="page">Create new post</li>
                     </ol>
                 </nav>
             </div>
         </div>
-        <form method="POST">        
-            <div class="row">
-                <div class="form-group col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                    <label for="title"><strong>Title</strong></label>
-                    <input type="text" class="form-control <?= isInvalid($validation_error,'title') ?>" id="title" name="title" value="<?= set_value('title') ?>" placeholder="Title">
-                    <?= invalidFeedback($validation_error, 'title') ?>
+        <form method="POST">
+            <div clas="card">
+                <div class="card-body bg-white">    
+                    <div class="row">
+                        <div class="form-group col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            <label for="title"><strong>Title</strong></label>
+                            <input type="text" class="form-control <?= isInvalid($validation_error,'title') ?>" id="title" name="title" value="<?= set_value('title') ?>" placeholder="Title">
+                            <?= invalidFeedback($validation_error, 'title') ?>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="form-group col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                            <label for="cod_category"><strong>Category</strong></label>
+                            <select class="form-control <?= isInvalid($validation_error,'cod_category') ?>" name="cod_category" id="cod_category">
+                                <option value="">Selecione</option>
+                                <?php foreach($list_categories as $row) { ?>
+                                    <?php $selected = ($row['cod_category'] == set_value('cod_category')) ? 'selected' : '' ?>
+                                    <option value="<?= $row['cod_category'] ?>" <?= $selected ?>><?= $row['category_description'] ?></option>    
+                                <?php } ?>
+                            </select>
+                            <?= invalidFeedback($validation_error, 'cod_category') ?>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="form-group col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            <label for="rg"><strong>Description</strong></label>
+                            <textarea class="form-control <?= isInvalid($validation_error,'description') ?>" name="description" id="description" rows="5">
+                                <?= set_value('description') ?>
+                            </textarea>
+                            <?= invalidFeedback($validation_error, 'description') ?>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="form-group col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            <label for="text"><strong>Post text</strong></label>
+                            <textarea class="form-control <?= isInvalid($validation_error,'text') ?>" name="text" id="text" rows="10">
+                                <?= set_value('text') ?>
+                            </textarea>
+                            <?= invalidFeedback($validation_error, 'text') ?>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div class="row">
-                <div class="form-group col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                    <label for="cod_category"><strong>Category</strong></label>
-                    <select class="form-control <?= isInvalid($validation_error,'cod_category') ?>" name="cod_category" id="cod_category">
-                        <option value="">Selecione</option>
-                        <?php foreach($list_categories as $row) { ?>
-                            <?php $selected = ($row['cod_category'] == set_value('cod_category')) ? 'selected' : '' ?>
-                            <option value="<?= $row['cod_category'] ?>" <?= $selected ?>><?= $row['category_description'] ?></option>    
-                        <?php } ?>
-                    </select>
-                    <?= invalidFeedback($validation_error, 'cod_category') ?>
-                </div>
-            </div>
-            <div class="row">
-                <div class="form-group col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                    <label for="rg"><strong>Description</strong></label>
-                    <textarea class="form-control <?= isInvalid($validation_error,'description') ?>" name="description" id="description" rows="5">
-                        <?= set_value('description') ?>
-                    </textarea>
-                    <?= invalidFeedback($validation_error, 'description') ?>
-                </div>
-            </div>
-            <div class="row">
-                <div class="form-group col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                    <label for="text"><strong>Post text</strong></label>
-                    <textarea class="form-control <?= isInvalid($validation_error,'text') ?>" name="text" id="text" rows="10">
-                        <?= set_value('text') ?>
-                    </textarea>
-                    <?= invalidFeedback($validation_error, 'text') ?>
-                </div>
-            </div>
-            <div class="row">
+            <div class="row mt-4">
                 <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-6">
                     <button class="btn btn-dark" type="submit"><i class="fa fa-save"></i>&nbsp;&nbsp;Save</button>
                 </div>
--- blog/app/Views/posts/list.php
+++ blog/app/Views/posts/list.php
@@ -4,54 +4,58 @@
         <div class="row">
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 mt-3">
                 <nav aria-label="breadcrumb">
-                    <ol class="breadcrumb">
+                    <ol class="breadcrumb bg-light">
                         <li class="breadcrumb-item active" aria-current="page">List of registered posts</li>
                     </ol>
                 </nav>
             </div>
         </div>
         <div class="row">
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                <?php if ($list_posts) { ?>
-                <div class="table-responsive">
-                    <table class="table table-bordered table-striped">
-                        <thead class="thead-ligth">
-                            <tr>
-                                <th scope="col">Title</th>
-                                <th class="text-center" scope="col">Category</th>
-                                <th class="text-center" scope="col">Register</th>
-                                <th class="text-center" scope="col">Edit</th>
-                                <th class="text-center" scope="col">Remove</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php foreach ($list_posts as $key => $value) { ?>
-                            <tr>
-                                <td><?= $value['title'] ?></td>
-                                <td class="text-center"><?= $value['category_description'] ?></td>
-                                <td class="text-center"><?= $value['dth_insert'] ?></td>
-                                <td class="text-center">
-                                    <a href="<?= site_url().'posts/edit/'.$value['cod_post'] ?>"  class="btn btn-small btn-info"
-                                        ><i class="fa fa-edit"></i>&nbsp;&nbsp;Edit
-                                    </a>
-                                </td>
-                                <td class="text-center">
-                                    <button type="button" class="btn btn-small btn-danger" onclick="removePost(<?= $value['cod_post'] ?>, '<?= $value['title'] ?>')">
-                                        <i class="fa fa-trash"></i>&nbsp;&nbsp;Remove
-                                    </button>
-                                </td>
-                            </tr>
-                            <?php } ?>
-                        </tbody>
-                    </table>
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 ">
+                <div clas="card">
+                    <div class="card-body bg-white">
+                        <?php if ($list_posts) { ?>
+                        <div class="table-responsive">
+                            <table class="table table-bordered table-striped">
+                                <thead class="thead-ligth">
+                                    <tr>
+                                        <th scope="col">Title</th>
+                                        <th class="text-center" scope="col">Category</th>
+                                        <th class="text-center" scope="col">Register</th>
+                                        <th class="text-center" scope="col">Edit</th>
+                                        <th class="text-center" scope="col">Remove</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php foreach ($list_posts as $key => $value) { ?>
+                                    <tr>
+                                        <td><?= $value['title'] ?></td>
+                                        <td class="text-center"><?= $value['category_description'] ?></td>
+                                        <td class="text-center"><?= $value['dth_insert'] ?></td>
+                                        <td class="text-center">
+                                            <a href="<?= site_url().'posts/edit/'.$value['cod_post'] ?>"  class="btn btn-small btn-info"
+                                                ><i class="fa fa-edit"></i>&nbsp;&nbsp;Edit
+                                            </a>
+                                        </td>
+                                        <td class="text-center">
+                                            <button type="button" class="btn btn-small btn-danger" onclick="removePost(<?= $value['cod_post'] ?>, '<?= $value['title'] ?>')">
+                                                <i class="fa fa-trash"></i>&nbsp;&nbsp;Remove
+                                            </button>
+                                        </td>
+                                    </tr>
+                                    <?php } ?>
+                                </tbody>
+                            </table>
+                        </div>
+                        <?php } else { ?>
+                            <p>There are no registered posts</p>
+                        <?php } ?>
+                    </div>
                 </div>
-                <?php } else { ?>
-                    <p>There are no registered posts</p>
-                <?php } ?>
             </div>
         </div>
         <div class="row">
-            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 mt-4">
                 <a href="<?= site_url().'posts/create' ?>" class="btn btn-dark"><i class="fa fa-plus"></i>&nbsp;&nbsp;create new post</a>
 
             </div>
